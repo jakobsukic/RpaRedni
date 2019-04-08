b1ab4c8f133872854dfbea6812e81206bcb0ba88
Jakob <sukic.jakob@gmail.com>
2019-04-08 09:21:50 +0200

    8.4.2019

---
+++ MVCStudenti/MVCStudenti/Controllers/StudentController.cs
@@ -0,0 +1,65 @@
+﻿using MVCStudenti.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCStudenti.Controllers{
+    public class StudentController : Controller{
+        // GET: Student
+        public ActionResult Index(){
+            var seznam = new List<Student>(){
+                    new Student(){
+                        StudentId=1,
+                        StudentName="John",
+                        Age=18
+                    },
+
+                    new Student(){
+                        StudentId=2,
+                        StudentName="Steve",
+                        Age=19
+                    },
+
+                    new Student(){
+                        StudentId=3,
+                        StudentName="Bill",
+                        Age=20
+                    },
+
+                    new Student(){
+                        StudentId=4,
+                        StudentName="Ron",
+                        Age=21
+                    },
+
+                    new Student(){
+                        StudentId=4,
+                        StudentName="Rob",
+                        Age=18
+                    },
+
+                    new Student(){
+                        StudentId=4,
+                        StudentName="Kris",
+                        Age=19
+                    },
+
+                    new Student(){
+                        StudentId=4,
+                        StudentName="Bob",
+                        Age=20
+                    },
+
+                    new Student(){
+                        StudentId=4,
+                        StudentName="Angel",
+                        Age=21
+                    },
+            };
+
+            return View(seznam);
+        }
+    }
+}
---
+++ MVCStudenti/MVCStudenti/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCStudenti.Models
+{
+    public class Student{
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+}
